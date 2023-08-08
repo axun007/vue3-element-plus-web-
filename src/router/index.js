f251e04d38d5b05d6796f678531ac604b16af121
Axchia <wx.liuchin@qq.com>
2023-08-08 12:16:08 +0800

    添加登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/homePage/HomeIndex.vue'
-
 const routes = [
   {
     path: '/',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "about" */ '../views/login/login.vue')
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView,
     redirect: '/panel/basedPanel',
     children: [
       {
         path: '/panel/basedPanel',
-        name: '',
+        name: 'panel.basedPanel',
         component: () => import(/* webpackChunkName: "about" */ '../views/panel/basePanel.vue')
       },
       {
         path: '/systemManagement/userManagement',
-        name: '',
+        name: 'systemManagement.userManagement',
         component: () => import(/* webpackChunkName: "about" */ '../views/systemManagement/userManagement.vue')
       }
     ]
-  }
+  },
 ]
-
 const router = createRouter({
   history: createWebHashHistory(),
   routes
