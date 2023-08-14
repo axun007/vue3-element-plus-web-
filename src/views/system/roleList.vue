39211b58edb956bcec023b5f993b888c218bad0d
Axchia <wx.liuchin@qq.com>
2023-08-14 11:08:48 +0800

    测试页面

--- src/views/system/roleList.vue
+++ src/views/system/roleList.vue
@@ -1,24 +1,19 @@
 <template>
-  <div>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
-    <h1>角色管理</h1>
+  <div class="role_box">
   </div>
-</template>
+ </template>
+ 
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style lang="scss" scoped>
+ .role_box{
+    width: 100%;
+    height: 100%;
+    background: rgba($color: rgb(108, 210, 109), $alpha: .7);
+ }
+ </style>
+ 
--- src/views/system/userList.vue
+++ src/views/system/userList.vue
@@ -1,24 +1,19 @@
 <template>
-  <div>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
-    <h1>用户管理</h1>
+  <div class="user_box">
   </div>
-</template>
+ </template>
+ 
+ <script>
+ export default {
+ 
+ }
+ </script>
+ 
+ <style lang="scss" scoped>
+ .user_box{
+    width: 100%;
+    height: 100%;
+    background: rgba($color: rgb(62, 134, 222), $alpha: .7);
+ }
+ </style>
+ 
