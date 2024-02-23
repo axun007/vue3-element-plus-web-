bf360ca934189228cb63ea718edce0e97e8f0f1f
Tomax <73932504+axun007@users.noreply.github.com>
2024-02-23 13:49:20 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,1 @@
-import { request } from './axios'
-export class UserService {
-    static async login(params) {   // 接口一
-        return request('/user/login',params, 'post')
-    }
-    static async registers(params) {   // 接口二
-        return request('/user/register',params, 'post')
-    }
-}
+
