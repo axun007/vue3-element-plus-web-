b5a49892f6a5e6305aa1c8fe5a6082e8e9250c3e
Tomax <73932504+axun007@users.noreply.github.com>
2024-02-23 13:49:42 +0800

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,12 +1,1 @@
-import Cookies from 'js-cookie'
-export function getToken() {
-  return Cookies.get(TokenKey)
-}
 
-export function setToken(token) {
-  return Cookies.set(TokenKey, token, { expires: 30 })
-}
-
-export function removeToken() {
-  return Cookies.remove(TokenKey)
-}
